f5177b2f44f146dc74ccfe2f03e0a210d9488a43
Adriana Broadnax <alwaysadrianna@gmail.com>
2022-02-12 13:06:15 -0500

    uvindex display

--- assets/displayWeather.js
+++ assets/displayWeather.js
@@ -8,6 +8,7 @@ var currentHumidty= $("#humidity");
 var currentWSpeed=$("#windSpeed");
 var currentUvindex= $("#uvIndex");
 var showCity=[];
+// API KEY
 var APIKey="a0aca8a89948154a4182dcecc780b513";
 
 
@@ -28,3 +29,53 @@ function find(c){
     }
   }
 
+    // CALLING API
+    function currentWeather(city){
+        //URL GIVEN
+        var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+        $.ajax({
+          url:queryURL,
+          method:"GET",
+        }).then(function(response){
+         })}
+
+         var weatherpic= response.weather[0].icon;
+         var picurl="https://openweathermap.org/img/wn/"+weatherpic +"@2x.png";
+
+        //  DATE
+        var date=new Date(response.dt*1000).toLocaleDateString();
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+picurl+">");
+
+        // Temp convert
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
+
+        // Humidity
+        $(currentHumidty).html(response.main.humidity+"%");
+
+        //  Wind
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph+"MPH");
+
+        // uvIndex Display
+        UVIndex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+          sCity=JSON.parse(localStorage.getItem("cityname"));
+          console.log(sCity);
+          if (sCity==null){
+            sCity=[];
+            sCity.push(city.toUpperCase()
+            );
+            localStorage.setItem("cityname",JSON.stringify(sCity));
+            addToList(city);
+          }
+          else {
+            if(find(city)>0){
+              sCity.push(city.toUpperCase());
+              localStorage.setItem("cityname",JSON.stringify(sCity));
+              addToList(city);
+            }
+          }
+        }
