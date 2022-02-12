c7728226848e3a8f68558ec4fc8ab8d283f77dc7
Adriana Broadnax <alwaysadrianna@gmail.com>
2022-02-12 12:16:17 -0500

    function to showCity and searchCity

--- assets/displayWeather.js
+++ assets/displayWeather.js
@@ -8,6 +8,8 @@ var currentHumidty= $("#humidity");
 var currentWSpeed=$("#windSpeed");
 var currentUvindex= $("#uvIndex");
 var showCity=[];
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+
 
 function find(c){
     for (var i=0; i<showCity.length; i++){
@@ -17,3 +19,12 @@ function find(c){
     }
     return 1;
   }
+
+  function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+      city=searchCity.val().trim();
+      currentWeather(city);
+    }
+  }
+
